bb580ffab0c02825be67fc593ed5ea3e8d41db2b
GayathriKaki <ghani319@gmail.com>
2019-11-28 15:58:26 +1100

    Add files via upload

---
+++ IRedPill.cs
@@ -0,0 +1,41 @@
+﻿using System; 
+ using System.Collections.Generic; 
+ using System.Linq; 
+ using System.Runtime.Serialization; 
+ using System.ServiceModel; 
+ using System.ServiceModel.Web; 
+ using System.Text; 
+ using RedPill.Model;
+
+
+namespace RedPill
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRedPill" in both code and config file together.
+     [ServiceContract(Namespace = "http://KnockKnock.readify.net", Name = "IRedPill")] 
+     public interface IRedPill
+     { 
+ 
+ 
+         [OperationContract] 
+         [WebGet(UriTemplate = "WhatIsYourToken")] 
+         Guid Token(); 
+ 
+ 
+         [FaultContract(typeof(ArgumentOutOfRangeException))] 
+         [OperationContract] 
+         [WebGet(UriTemplate = "FibonacciNumber/{n}")] 
+         long Fibonacci(long n); 
+ 
+ 
+         [OperationContract] 
+         [WebGet] 
+         TriangleType TraingleType(int a, int b, int c); 
+ 
+ 
+         [OperationContract] 
+         [FaultContract(typeof(ArgumentNullException))] 
+         [WebGet] 
+         string ReverseWords(string s); 
+     } 
+
+}
---
+++ RedPill.svc.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using RedPill.Model;
+using RedPill.Services;
+
+namespace RedPill
+{
+   [ServiceBehavior(Namespace = "http://KnockKnock.readify.net")] 
+     public class RedPill : IRedPill
+    { 
+         private FibonacciService _fibonacciService; 
+         private TriangleService _triangleService; 
+         private ReverseWordsService _reverseWordsService; 
+ 
+         public FibonacciService FibonacciService 
+         { 
+             get 
+             { 
+                 if (_fibonacciService == null) 
+                 { 
+                     _fibonacciService = new FibonacciService(); 
+                 } 
+                 return _fibonacciService; 
+             } 
+         } 
+
+
+         public TriangleService TriangleService
+         { 
+             get 
+             { 
+                 if (_triangleService == null) 
+                 { 
+                     _triangleService = new TriangleService(); 
+                 } 
+                 return _triangleService; 
+             } 
+         } 
+ 
+
+         public ReverseWordsService ReverseWordsService
+         { 
+             get 
+             { 
+                 if (_reverseWordsService == null) 
+                 { 
+                     _reverseWordsService = new ReverseWordsService(); 
+                 } 
+                 return _reverseWordsService; 
+             } 
+         } 
+ 
+ 
+         public Guid Token()
+         { 
+             return new Guid("00000000-0000-0000-0000-000000000000"); 
+         } 
+ 
+ 
+         public long Fibonacci(long n)
+         { 
+             try 
+             { 
+                 return FibonacciService.GetNum(n); 
+             } 
+             catch (ArgumentOutOfRangeException e) 
+             { 
+                 throw new FaultException<ArgumentOutOfRangeException>(e, e.Message); 
+             } 
+              
+         } 
+ 
+ 
+         public TriangleType TraingleType(int a, int b, int c)
+         { 
+             return TriangleService.GetTriaType(a, b, c); 
+         } 
+ 
+ 
+         public string ReverseWords(string s)
+         { 
+             try 
+             { 
+                 return ReverseWordsService.ReverseWords(s); 
+             } 
+             catch (ArgumentNullException e) 
+             { 
+ 
+ 
+                 throw new FaultException<ArgumentNullException>(e,e.Message); 
+             } 
+              
+         } 
+     } 
+
+ 
+      
+
+}
